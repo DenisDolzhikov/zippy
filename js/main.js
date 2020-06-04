6f0174da20108ddcffa812abe61c7b2694bbdd1a
Denis <35024218+DenisDolzhikov@users.noreply.github.com>
2020-06-04 15:23:41 +0300

    save

---
+++ js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+{
+    let stickyNav = document.querySelector('.sticky-nav');
+
+    if (window.scrollY > 120) {
+        stickyNav.classList.add('offset');
+    }
+    
+    function onScroll() {
+        window.addEventListener('scroll', callbackFunc);
+
+        function callbackFunc() {
+            let y = window.pageYOffset;
+
+            if (y > 120) {
+                stickyNav.classList.add('scroll');
+                stickyNav.style.transition = '0.5s';
+            } else {
+                stickyNav.classList.remove('scroll');
+                stickyNav.classList.remove('offset');
+            }
+        }
+    }
+
+    window.onload = function() {
+        onScroll();
+    }
+}
